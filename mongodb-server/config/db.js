e4225a7245fa518645aa78e6b7c359569f7a5900
safi50 <safi.shah01@outlook.com>
2024-04-03 02:20:48 +0500

    updated env var

--- mongodb-server/config/db.js
+++ mongodb-server/config/db.js
@@ -5,7 +5,7 @@ const dotenv = require('dotenv');
 dotenv.config(); // configuring .env 
 
 const connectDB = async () => {
-    await mongoose.connect(process.env.MONGODB_URL, {
+    await mongoose.connect(process.env.MONGODB_URI, {
     }).then(() => {
         console.log("Connected to Database Successfully!");
     }).catch((err) => {
